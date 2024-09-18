61b24543992b8060c76e22db736e4a3dfd300cce
Oximiron14 <oximiron149@gmail.com>
2024-09-18 09:26:59 +0300

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,24 @@
+let x = +prompt("ВВЕДИТЕ ЗНАЧЕНИЕ x");
+
+let y = +prompt("ВВЕДИТЕ ЗНАЧЕНИЕ y");
+
+let e = +prompt("Введите значение e");
+
+let a = +prompt("Введите значение a");
+
+let b = +prompt("Введите число b ");
+
+let n =0;
+
+let g;
+
+if (x * y > 0) {
+    let z = Math.pow(1-e,x*y+ a*b)
+    console.log(z);
+}else if (n = x * y) {
+    let c = (b-Math.min(a*x,y))
+    console.log(c)
+}else if(x*y<0) {
+    v = Math.max(Math.pow(x,3)*Math.pow(e,y)*Math.sqrt(Math.pow(y,2)))
+    console.log(v)
+}
