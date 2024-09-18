28cb0e4df89d5676b3d26fa9c9cd880fc6afd574
Oximiron14 <oximiron149@gmail.com>
2024-09-18 09:23:50 +0300

    SADASDSADA
    D

---
+++ index.js
@@ -0,0 +1,8 @@
+let x = +prompt("ВВЕДИТЕ ЗНАЧЕНИЕ x");
+
+let y = +prompt("ВВЕДИТЕ ЗНАЧЕНИЕ y");
+
+let c = Math.pow((y * Math.pow(x,2)-1),2)/2 * (Math.pow(Math.cos(y),2) - Math.sin(Math.pow(x,2)));
+
+console.log(c);
+
